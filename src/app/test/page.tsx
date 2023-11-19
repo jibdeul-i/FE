b3164e2fcb62999269f20943c96d5237239356ed
caretbio_sjune <sjune06@caretbio.com>
2023-11-19 13:04:13 +0900

    map conponent use client

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,13 @@
+import JejuMap from "@/components/Map";
+
 const TestPage = () => {
   return (
     <>
       <div className={"bg-purple-500 h-[1000px] flex flex-col"}>
         <div>page-content1</div>
-        <h2 className="mt-[150px] mx-auto">스크롤을 해보세여</h2>
+        <h2 className="mt-[150px] mx-auto">
+          <JejuMap />
+        </h2>
       </div>
       <div className="bg-black h-[1000px] text-white flex flex-col">
         <div>page-content2</div>
