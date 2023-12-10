8df0a6d875c4916276b1107f407a8f6dbe7e3da2
MinjooKim <kingwinju@gmail.com>
2023-12-10 16:04:11 +0900

    Feat implementing map list

--- src/components/CategoryResultList.tsx
+++ src/components/CategoryResultList.tsx
@@ -1,15 +1,15 @@
 import { touristSpots } from "@/types/touristSpots"
 
-interface CategoryResultListProps = {
+interface CategoryResultListProps {
   spots: TouristSpot[];
 }
 
 const CategoryResultList = ({ spots }: CategoryResultListProps) => {
   return (
-    <div className="">
+    <div className="fixed inset-x-0 bottom-12 z-50 p-4 bg-white shadow-lg overflow-y-auto w-[520px] max-h-32">
       {spots.map(spot => (
-        <div key={spot.id}>
-          <h3>{spot.name}</h3>
+        <div key={spot.name} className="p-2 hover:bg-gray-100">
+          <h3 className="text-md font-semibold">{spot.name}</h3>
         </div>
       ))}
     </div>
